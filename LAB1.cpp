4581bc026b623c261310ef441facdfcfb5b8a33b
RGD006 <mysak6288@gmail.com>
2025-10-26 14:46:39 +0200

    chore: return to first commit

--- LAB1.cpp
+++ LAB1.cpp
@@ -134,29 +134,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     } break;
     case WM_PAINT: {
       PAINTSTRUCT ps;
-      constexpr size_t polygonNumber = 5;
-      HDC hdc                        = BeginPaint(hWnd, &ps);
-      HGDIOBJ object                 = nullptr;
-
-      SelectObject(hdc, GetStockObject(DC_BRUSH));
-      SelectObject(hdc, GetStockObject(DC_PEN));
-
-      SetDCBrushColor(hdc, RGB(255, 0, 0));
-      Rectangle(hdc, 50, 50, 200, 200);
-
-      SetDCBrushColor(hdc, RGB(0, 255, 0));
-      Ellipse(hdc, 300, 50, 500, 200);
-
-      SetDCBrushColor(hdc, RGB(0, 0, 255));
-      POINT pollygonPoints[polygonNumber] = {
-          {500, 50},
-          {550, 100},
-          {550, 200},
-          {700, 100},
-          {700, 50},
-      };
-      Polygon(hdc, pollygonPoints, polygonNumber);
-
+      HDC hdc = BeginPaint(hWnd, &ps);
+      // TODO: Add any drawing code that uses hdc here...
       EndPaint(hWnd, &ps);
     } break;
     case WM_DESTROY:
