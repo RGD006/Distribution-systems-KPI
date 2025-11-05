3d1849064f1ae2aeb3b652fbea48e0eaf7eb9144
RGD006 <mysak6288@gmail.com>
2025-11-05 20:24:54 +0200

    feat: add FileInfo class

--- ThreadFunctions.cpp
+++ ThreadFunctions.cpp
@@ -1,15 +1,45 @@
 #include "ThreadFunctions.h"
 
-DINFO dataInfo = {0};
+static DWORD WINAPI ReadFile(LPVOID filePath);
+static DWORD WINAPI WriteStats(LPVOID dataArray);
+static DWORD WINAPI OutputStats(LPVOID window);
+
+EVENTFUNC::EVENTFUNC(HANDLE _sEvent, HANDLE _fEvent)
+{
+  sEvent = _sEvent;
+  fEvent = _fEvent;
+}
+
+EVENTFUNC::EVENTFUNC()
+{
+  sEvent = nullptr;
+  fEvent = nullptr;
+}
+
+FileInfo::FileInfo()
+    : efReadFile(CreateSemaphore(NULL, 1, 1, NULL),
+                 CreateThread(NULL, 0, ReadFile, NULL, 0, NULL)),
+      efWriteStats(CreateSemaphore(NULL, 1, 1, NULL),
+                 CreateThread(NULL, 0, WriteStats, NULL, 0, NULL)),
+      efOutputStats(CreateSemaphore(NULL, 1, 1, NULL),
+                 CreateThread(NULL, 0, OutputStats, NULL, 0, NULL))
+{
+}
 
 DWORD WINAPI ReadFile(LPVOID filePath)
 {
+
+  return 0;
 }
 
 DWORD WINAPI WriteStats(LPVOID dataArray)
 {
+
+  return 0;
 }
 
-DWORD WINAPI OutputStats(LPVOID Label)
+DWORD WINAPI OutputStats(LPVOID window)
 {
+
+  return 0;
 }
--- ThreadFunctions.h
+++ ThreadFunctions.h
@@ -1,17 +1,31 @@
 #pragma once
 #include <windows.h>
-#include <string.h>
+#include <string>
 #include <cstdint>
 
-struct DINFO
-{
+struct EVENTFUNC {
+  HANDLE sEvent;  // event semaphore
+  HANDLE fEvent;  // event function
+  EVENTFUNC(HANDLE _sEvent, HANDLE _fEvent);
+  EVENTFUNC();
+};
+
+struct FILESTATS {
   uint32_t numberLines;
   uint32_t numberWords;
   uint32_t numberCharacters;
 };
 
-extern DINFO dataInfo;
+class FileInfo {
+private:
+  HWND *windowHandler;
+  FILESTATS fileStats;
+  EVENTFUNC efReadFile;
+  EVENTFUNC efWriteStats;
+  EVENTFUNC efOutputStats;
 
-DWORD WINAPI ReadFile(LPVOID filePath);
-DWORD WINAPI WriteStats(LPVOID dataArray);
-DWORD WINAPI OutputStats(LPVOID Label);
+public:
+  FileInfo();
+  void startGetInfo(std::wstring path);
+  FILESTATS getInfo(void);
+};
